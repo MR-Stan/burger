108de96080707d395b655ab60433caebbe5f7ffe
MR-Stan <michaelrstanley87@gmail.com>
2019-12-09 20:05:32 -0600

    updated server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+// npm express
+const express = require('express');
+
+// npm express handlebars
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 8080;
+
+const routes = require('./controllers/burgers_controller.js');
+
+const app = express();
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// use static content from public
+app.use(express.static("public"));
+
+app.use(routes);
+
+// start server
+app.listen(PORT, function () {
+    console.log('Server listenting on: http://localhost:' + PORT);
+});
+
+
