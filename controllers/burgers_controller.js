b1a8d5375b0e1c3d9a31add34b88dd59c3a099dc
MR-Stan <michaelrstanley87@gmail.com>
2019-12-12 17:55:51 -0600

    added get, port, and put to burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,10 +4,33 @@ const router = express.Router();
 
 const burger = require('../models/burger.js');
 
-// router.get
+router.get('/', function (req, res) {
+    burger.selectAll(function (data) {
+        let hbsObj = { burgers: data }
+        console.log(hbsObj);
+        res.render('index', hbsObj);
+    });
+});
 
-// router.post
+// burgers
+router.post('/api/burgers', function (req, res) {
+    burger.insertBurger(["burger_name", "devoured"], [req.body.name, req.body.devoured], function (result) {
+        console.log(result);
+        res.json({ id: result.id });
+    });
+});
 
-// router.put
+router.put('/api/burgers/:id', function (req, res) {
+    let condition = 'id = ' + red.params.id;
+    console.log('condition', condition);
+    burger.updateBurger({ devoured: red.body.devoured }, condition, function (result) {
+        if (!result.changedRows) {
+            return res.status(404).end();
+        }
+        else {
+            res.status(200).end();
+        }
+    });
+});
 
 module.exports = router;
