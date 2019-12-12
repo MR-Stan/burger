fdbda5c1775a794ea6e17d1861ef01138dbb63d4
MR-Stan <michaelrstanley87@gmail.com>
2019-12-11 18:59:18 -0600

    added db connection, insertBurger, and updateBurger to orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,23 +1,40 @@
 const connection = require('./connection.js');
 
-
+// connect to db
+connection.connect(function (err) {
+    if (err) {
+        console.log("Error connecting: " + err.stack);
+        return;
+    }
+    console.log("Connected as id " + connection.threadId);
+});
 
 const orm = {
     // select all information from a given table
-    selectAll = function (table, cb) {
+    selectAll = function (table, callback) {
         let sqlQuery = "SELECT * FROM " + table + ";";
         connection.query(sqlQuery, function (err, res) {
             if (err) throw err;
-            cb(res);
+            callback(res);
         });
     },
     //
-    insertOne = function () {
-
+    insertBurger = function (table, burger_name, callback) {
+        let sqlQuery = "INSERT INTO " + table + "VALUES (" + burger_name + ");";
+        console.log(sqlQuery);
+        connection.query(sqlQuery, function (err, res) {
+            if (err) throw err;
+            callback(res);
+        });
     },
     //
-    updateOne = function () {
-
+    updateBurger = function (id, callback) {
+        connection.query("UPDATE burgers SET ? WHERE ?",
+            [{ devoured: true }, { id: id }],
+            function (err, result) {
+                if (err) throw err;
+                callback(res);
+            });
     }
 }
 
